192dc6dd0d19a3387f37c0952b04160005a5b58f
Bob-Coding <bobgunsing@gmail.com>
2020-05-29 00:06:05 +0200

    added ticketdetailspage

--- src/Components/Ticket/TicketDetails/TicketDetails.js
+++ src/Components/Ticket/TicketDetails/TicketDetails.js
@@ -1,7 +1,39 @@
-// import React from "react";
+import React from "react";
 
-// const ticketDetails = () => {
-//   return null;
-// };
+import Button from "../../UI/Button/Button";
+import Aux from "../../../hoc/Aux/Aux";
+import classes from "./TicketDetails.module.css";
 
-// export default ticketDetails;
+const TicketDetails = (props) => {
+  return (
+    <Aux>
+      {props.ticket && (
+        <div className={classes.TicketDetails}>
+          <h1>{props.ticket.name}</h1>
+          <p>
+            <i>Date: {props.ticket.startDate} - </i>
+            <i>{props.ticket.endDate}</i>
+          </p>
+          <p>
+            <i>description: {props.ticket.description}</i>
+          </p>
+          <p className={classes.TicketPicture}>
+            <img src={props.ticket.picture} alt="ticket" />
+          </p>
+        </div>
+      )}
+      {props.hasCreatedTicket(props.userId) && (
+        <div>
+          <Button btnType="Danger" clicked={props.onDelete}>
+            DELETE TICKET
+          </Button>
+          <Button btnType="Success" clicked={props.onEdit}>
+            EDIT TICKET
+          </Button>
+        </div>
+      )}
+    </Aux>
+  );
+};
+
+export default TicketDetails;
--- src/Containers/Events/EventDetails/Tickets/TicketDetails/TicketDetails.js
+++ src/Containers/Events/EventDetails/Tickets/TicketDetails/TicketDetails.js
@@ -1,1 +1,169 @@
-//
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { checkValidity } from "../../../../../Components/Validation/CheckValidity";
+import CreateTicketForm from "../../../../../Components/Ticket/CreateTicketForm/CreateTicketForm";
+import TicketDetails from "../../../../../Components/Ticket/TicketDetails/TicketDetails";
+import Spinner from "../../../../../Components/UI/Spinner/Spinner";
+import * as actions from "../../../../../store/actions/index";
+
+class TicketDetailsContainer extends Component {
+  state = {
+    showTicketForm: false,
+    ticketForm: {
+      picture: {
+        elementType: "input",
+        elementConfig: {
+          type: "url",
+          placeholder: "Ticket Picture",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      price: {
+        elementType: "input",
+        elementConfig: {
+          type: "number",
+          placeholder: "Ticket Price",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      description: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Ticket Description",
+        },
+        value: "",
+        validation: {
+          required: false,
+        },
+        valid: false,
+        touched: false,
+      },
+    },
+  };
+
+  componentDidMount() {
+    this.props.onFetchTicket(Number(this.props.match.params.id));
+  }
+
+  onEdit = () => {
+    this.setState({
+      editMode: !this.state.editMode,
+    });
+  };
+
+  inputChangedHandler = (event, formElement) => {
+    const updatedTicketForm = {
+      ...this.state.ticketForm,
+    };
+    const updatedFormElement = {
+      ...updatedTicketForm[formElement],
+    };
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+    updatedFormElement.touched = true;
+    updatedTicketForm[formElement] = updatedFormElement;
+
+    let formIsValid = true;
+    for (let formElement in updatedTicketForm) {
+      formIsValid = updatedTicketForm[formElement].valid && formIsValid;
+    }
+    this.setState({ ticketForm: updatedTicketForm, formIsValid: formIsValid });
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    const formData = {};
+    for (let formElement in this.state.ticketForm) {
+      formData[formElement] = this.state.ticketForm[formElement].value;
+    }
+    const formToSubmit = {
+      userId: this.props.userId,
+      ticketData: formData,
+    };
+    this.setState({ editmode: false });
+    this.props.onUpdateTicket(this.props.ticket.id, formToSubmit.ticketData);
+  };
+
+  deleteTicketHandler = () => {
+    this.props.onDeleteTicket(this.props.ticket.id, this.props.token);
+    this.props.history.push("/");
+  };
+
+  hasCreated = (id) => {
+    if (this.props.ticket && this.props.ticket.userId && id) {
+      return this.props.ticket.userId === id;
+    }
+  };
+
+  render() {
+    let ticket = <Spinner />;
+    if (!this.props.loading) {
+      ticket = (
+        <TicketDetails
+          onEdit={this.onEdit}
+          changed={this.inputChangedHandler}
+          onSubmitTicket={this.submitHandler}
+          values={this.state}
+          editMode={this.state.editMode}
+          onDelete={this.deleteTicketHandler}
+          ticket={this.props.ticket}
+          hasCreatedTicket={this.hasCreated}
+          userId={this.props.userId}
+        />
+      );
+    }
+
+    let ticketform = null;
+    if (this.state.editMode) {
+      ticketform = (
+        <CreateTicketForm
+          onSubmitTicket={this.submitHandler}
+          changed={this.inputChangedHandler}
+          values={this.state}
+        />
+      );
+    }
+
+    return (
+      <div>
+        <h1>Ticket</h1>
+        {ticket}
+        {ticketform}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  ticket: state.ticket.ticket,
+  loading: state.ticket.loading,
+  userId: state.auth.userId,
+  token: state.auth.token,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchTicket: (id) => dispatch(actions.fetchTicket(id)),
+    onDeleteTicket: (id, token) => dispatch(actions.deleteTicket(id, token)),
+    onUpdateTicket: (id, data) => dispatch(actions.updateTicket(id, data)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TicketDetailsContainer);
